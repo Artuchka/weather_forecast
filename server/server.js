3d2c34097fd5f94578d9d0cc6ed87f659b960c09
Temka Artemka <yandex949@gmail.com>
2023-02-12 13:19:36 +0300

    init

--- server/server.js
+++ server/server.js
@@ -9,35 +9,37 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 // if api does not work
-// const weatherData = require("./example.json")
+const weatherData = require("./example.json")
+
+// const baseUrl = "https://api.openweathermap.org/data/2.5/onecall"
+// const baseUrl = "https://api.openweathermap.org/data/2.5/weather"
 
 app.get("/weather", (req, res) => {
 	const { lat, lon } = req.query
+	const exclude = "minutely,alerts"
+	const units = "metric"
+	const appid = process.env.API_KEY
 
-	axios
-		.get(`https://api.openweathermap.org/data/2.5/onecall`, {
-			params: {
-				lat,
-				lon,
-				appid: process.env.API_KEY,
-				units: "metric",
-				exclude: "minutely,alerts",
-			},
-		})
-		.then(({ data }) => {
-			res.json({
-				current: parseCurrentWeather(data),
-				daily: parseDailyWeather(data),
-				hourly: parseHourlyWeather(data),
-			})
-		})
-		.catch((e) => {
-			console.log(e)
-			res.sendStatus(500)
-		})
+	// axios
+	// 	.get(
+	// 		`${baseUrl}?lat={${lat}}&lon={${lon}}&exclude={${exclude}}&appid={${appid}}`
+	// 	)
+	// 	.then(({ data }) => {
+	res.json({
+		current: parseCurrentWeather(weatherData),
+		daily: parseDailyWeather(weatherData),
+		hourly: parseHourlyWeather(weatherData),
+	})
+	// })
+	// .catch((e) => {
+	// 	console.log(e)
+	// 	res.sendStatus(500)
+	// })
 })
 
-app.listen(3001)
+app.listen(3001, () => {
+	console.log("listening on 3001")
+})
 
 function parseCurrentWeather({ current, daily }) {
 	const { temp: currentTemp, weather, wind_speed } = current
